f4b4e44f739e95de684f1eb04c4b7dbcf87f3bf3
Lucasdfg <cervantes.lucas99@gmail.com>
2019-10-21 18:28:08 -0500

    new challenge

---
+++ hackerrank/problemSolving/algorithms/pickingNumbers.js
@@ -0,0 +1,22 @@
+/* return an integer that represents the length of the longest array that can be created.
+
+pickingNumbers has the following parameter(s):
+
+a: an array of integers */
+function pickingNumbers(a) {
+    var mySet= new Set();
+    var maxCount=0;
+    for(var i=0; i<a.length; i++){
+        var count=0;
+        if(!mySet.has(a[i])){
+            mySet.add(a[i]);
+            for(var j=0; j<a.length; j++){
+                if(a[j]==a[i] || a[j]==a[i]+1){
+                    count++;
+                }       
+            }
+            maxCount= Math.max(maxCount,count);
+        }
+    }
+    return maxCount;
+}
