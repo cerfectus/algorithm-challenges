97085fd6cf7f0788778bf7e723701666fc9fc38e
Lucasdfg <cervantes.lucas99@gmail.com>
2019-10-25 15:29:12 -0500

    new

---
+++ hackerrank/problemSolving/algorithms/encryption.js
@@ -0,0 +1,28 @@
+/* return a single string composed as described.
+
+encryption has the following parameter(s):
+
+s: a string to encrypt */
+function encryption(s) {
+    let sLength = s.length
+      let numberOfRows = Math.floor(Math.sqrt(sLength))
+      let numberOfCols = Math.ceil(Math.sqrt(sLength))
+    
+      while (numberOfRows * numberOfCols < sLength) {
+        numberOfRows++
+      }
+    
+      let output = ''
+    
+      for (let i = 0; i < numberOfCols; i++) {
+        for (let j = 0; j < numberOfRows; j++) {
+          let letter = s[i + j * numberOfCols]
+          output += letter != null ? letter : ''
+        }
+    
+        output += ' '
+      }
+    
+      return output
+    
+    }
