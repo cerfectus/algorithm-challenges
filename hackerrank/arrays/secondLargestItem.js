45923cf328f3bd246188309497f97cc398af6a61
Lucasdfg <cervantes.lucas99@gmail.com>
2019-10-31 08:42:50 -0600

    added arrays file

---
+++ hackerrank/arrays/secondLargestItem.js
@@ -0,0 +1,16 @@
+/*Return the value of the second largest number in the  array.*/
+
+function getSecondLargest(nums) {
+    let first = nums[0]; let second = -1;
+    for(let i =0;i<nums.length;i++){
+        if(nums[i] > first){
+            second = first
+            first = nums[i]
+        }
+        if(nums[i]> second && nums[i]<first){
+            second = nums[i]
+        }
+    }
+    return second
+   
+}
